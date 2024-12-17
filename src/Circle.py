565cdd8fc3938b873c2d7237f69f7a0b3a81c414
bor1sor <belov.p.1994@gmail.com>
2024-12-17 13:14:23 +0300

    Figure (Circle.py, Rectangle.py, Square.py, Triangle.py)

---
+++ src/Circle.py
@@ -0,0 +1,33 @@
+import math
+
+class Circle:
+    def __init__(self, radius):
+        if radius <= 0:
+            raise ValueError
+        self.radius = radius
+
+    @property
+    def area(self):
+        return math.pi * (self.radius ** 2)
+
+class Square(Circle):
+    def __init__(self, side_a):
+        super().__init__(side_a / math.sqrt(2))
+        self.side_a = side_a
+
+    @property
+    def area(self):
+        return self.side_a ** 2
+
+    def add_area(self, figure):
+        if not isinstance(figure, (Circle, Square)):
+            raise ValueError
+        return self.area + figure.area
+
+try:
+    c = Circle(5)
+    s = Square(5)
+
+    total_area = s.add_area(c)
+except ValueError as e:
+    pass
---
+++ src/Triangle.py
@@ -0,0 +1,29 @@
+import math
+
+class Triangle:
+    def __init__(self, side_a, side_b, side_c):
+        if (side_a + side_b <= side_c) or (side_a + side_c <= side_b) or (side_b + side_c <= side_a):
+            raise ValueError("Треугольник с этими сторонами создать нельзя")
+        self.side_a = side_a
+        self.side_b = side_b
+        self.side_c = side_c
+
+    @property
+    def area(self):
+        s = (self.side_a + self.side_b + self.side_c) / 2
+        return math.sqrt(s * (s - self.side_a) * (s - self.side_b) * (s - self.side_c))
+
+class Square:
+    def __init__(self, side_a):
+        self.side_a = side_a
+
+    @property
+    def area(self):
+        return self.side_a ** 2
+
+
+t = Triangle(13, 14, 15)
+s = Square(5)
+
+total_area = t.area + s.area
+
