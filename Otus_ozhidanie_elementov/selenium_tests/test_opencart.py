abeab225b051ab3d2b43819f98da046d2cc9c31f
Maxim <maximchernov2018@gmail.com>
2025-03-18 18:40:17 +0300

    Delete Otus_ozhidanie_elementov directory

--- Otus_ozhidanie_elementov/selenium_tests/test_opencart.py
+++
@@ -1,9 +0,0 @@
-def test_open_opencart(browser, base_url):
-    # Открываем базовый URL
-    browser.get(base_url)
-
-    # Проверяем, что заголовок страницы не пустой
-    assert browser.title != "", "Заголовок страницы пустой"
-
-    # Выводим фактический заголовок для отладки
-    print(f"Фактический заголовок: {browser.title}")
